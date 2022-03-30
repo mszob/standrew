a0c95e0c98e700f17281b5572f9657fdcf8fc6fa
picturamundi <72834440+picturamundi@users.noreply.github.com>
2022-03-30 13:40:50 -0500

    dynamic favicon

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 // this one is jut to wait for the page to load
 document.addEventListener('DOMContentLoaded', () => {
     const themeStylesheet = document.getElementById('theme');
+    const faviconTheme = document.getElementById('favicon');
     const storedTheme = localStorage.getItem('theme');
+    const storedFavicon = localStorage.getItem('favicon');
     if(storedTheme){
         themeStylesheet.href = storedTheme;
     }
+    if(storedFavicon){
+       faviconTheme.href = storedFavicon;
+  }
     const themeToggle = document.getElementById('theme-toggle');
     themeToggle.addEventListener('click', () => {
         // if it's light -> go dark
         if(themeStylesheet.href.includes('light')){
             themeStylesheet.href = 'dark.css';
+            faviconTheme.href = 'assets/icons/favicon-dark.png';
             //themeToggle.innerText = 'Switch to light mode';
         //} else if(themeStylesheet.href.includes('dark')){
           //themeStylesheet.href = 'minimal.css';
@@ -17,10 +23,12 @@ document.addEventListener('DOMContentLoaded', () => {
         } else {
             // if it's dark -> go light
             themeStylesheet.href = 'light.css';
+            faviconTheme.href = 'assets/icons/favicon.png';
             //themeToggle.innerText = 'Switch to dark mode';
         }
         // save the preference to localStorage
         localStorage.setItem('theme',themeStylesheet.href)
+        localStorage.setItem('favicon',faviconTheme.href)
     })
 })
 
